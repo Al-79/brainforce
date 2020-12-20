9edcd4f5828941dba2c322f68b11de3216ca3336
Al-79 <AlorhZ@gmail.com>
2020-12-20 10:09:13 +0900

    make-files

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'pry-rails'
 group :production do
   gem 'unicorn', '5.4.1'
 end
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :books
+  devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: 'fronts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
