b490ee9e31ea3d4c3db83f4dac3f0ff7e89926be
Al-79 <AlorhZ@gmail.com>
2021-01-10 17:20:52 +0900

    wordbook-visible
    単語帳を表示

--- app/javascript/concentrations.js
+++ app/javascript/concentrations.js
@@ -1,9 +1,44 @@
 document.addEventListener("turbolinks:load", function () {
+
+    // シャッフル
+    function shuffle(){
+      // 0からnまでの数列と空の数列を作る
+      indexArr = Array.from({ length: how_many_card }).map((_, index) => index)
+      randomArr = []
+      // 空の数列に数を移し、ランダム数列にする
+      while (indexArr.length > 0) {
+        n = indexArr.length;
+        k = Math.floor(Math.random() * n);
+      
+        randomArr.push(indexArr[k]);
+        indexArr.splice(k, 1);
+      }
+      // 要素を格納
+      var textArr = Array(how_many_card)
+      var colorArr = Array(how_many_card)
+      var idArr = Array(how_many_card)
+      var displayArr = Array(how_many_card)
+      for (let step = 0; step < how_many_card; step++){
+        textArr[step]=$($('.concentration--card')[step]).find('.concentration--text').text()
+        colorArr[step]=$($('.concentration--card')[step]).find('.concentration--text').css('color')
+        idArr[step]=$($('.concentration--card')[step]).find('.concentration--pair').attr('id')
+        displayArr[step]=$($('.concentration--tail')[step]).css('display')
+      }
+      // ランダム数列に合わせて要素を割り当てる
+      for (let step = 0; step < how_many_card; step++){
+        $($('.concentration--card')[step]).find('.concentration--text').text(textArr[randomArr[step]])
+        $($('.concentration--card')[step]).find('.concentration--text').css('color',colorArr[randomArr[step]])
+        $($('.concentration--card')[step]).find('.concentration--pair').attr('id', idArr[randomArr[step]])
+        $($('.concentration--tail')[step]).css('display',displayArr[randomArr[step]])
+      }
+    }
   var isSelect = false
   var isSelect = false
   var check_color = 'rgb(255, 165, 0)'// orange
   var normal_color = 'rgb(255, 255, 0)'// yellow
   var how_many_card = $('.concentration--card').length
+  shuffle()// いきなりシャッフル
+
   $(function() {
     // カードを選択
     $('.concentration--tail').on('click', function() {
@@ -50,37 +85,9 @@ document.addEventListener("turbolinks:load", function () {
       }, 3000);
     })
 
-    // シャッフル
     $('#shuffle_card').on('click', function() {
-      // 0からnまでの数列と空の数列を作る
-      indexArr = Array.from({ length: how_many_card }).map((_, index) => index)
-      randomArr = []
-      // 空の数列に数を移し、ランダム数列にする
-      while (indexArr.length > 0) {
-        n = indexArr.length;
-        k = Math.floor(Math.random() * n);
-      
-        randomArr.push(indexArr[k]);
-        indexArr.splice(k, 1);
-      }
-      // 要素を格納
-      var textArr = Array(how_many_card)
-      var colorArr = Array(how_many_card)
-      var idArr = Array(how_many_card)
-      var displayArr = Array(how_many_card)
-      for (let step = 0; step < how_many_card; step++){
-        textArr[step]=$($('.concentration--card')[step]).find('.concentration--text').text()
-        colorArr[step]=$($('.concentration--card')[step]).find('.concentration--text').css('color')
-        idArr[step]=$($('.concentration--card')[step]).find('.concentration--pair').attr('id')
-        displayArr[step]=$($('.concentration--tail')[step]).css('display')
-      }
-      // ランダム数列に合わせて要素を割り当てる
-      for (let step = 0; step < how_many_card; step++){
-        $($('.concentration--card')[step]).find('.concentration--text').text(textArr[randomArr[step]])
-        $($('.concentration--card')[step]).find('.concentration--text').css('color',colorArr[randomArr[step]])
-        $($('.concentration--card')[step]).find('.concentration--pair').attr('id', idArr[randomArr[step]])
-        $($('.concentration--tail')[step]).css('display',displayArr[randomArr[step]])
-      }
+      shuffle()
     })
+  
   })
 })
