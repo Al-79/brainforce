74227ddd5b9509482a66b6926d7d2648211922f6
Al-79 <AlorhZ@gmail.com>
2021-02-03 01:14:05 +0900

    concentration_format
    神経衰弱のフォーマット変更

--- app/javascript/concentrations.js
+++ app/javascript/concentrations.js
@@ -19,46 +19,46 @@ document.addEventListener("turbolinks:load", function () {
     var idArr = Array(how_many_card)
     var displayArr = Array(how_many_card)
     for (let step = 0; step < how_many_card; step++){
-      textArr[step]=$($('.concentration--card')[step]).find('.concentration--text').text()
-      colorArr[step]=$($('.concentration--card')[step]).find('.concentration--text').css('color')
-      idArr[step]=$($('.concentration--card')[step]).find('.concentration--id').attr('id')
-      displayArr[step]=$($('.concentration--tail')[step]).css('display')
+      textArr[step]=$($('.card__cell')[step]).find('.concentration__text').text()
+      colorArr[step]=$($('.card__cell')[step]).find('.concentration__text').css('color')
+      idArr[step]=$($('.card__cell')[step]).find('.concentration__id').attr('id')
+      displayArr[step]=$($('.concentration__tail')[step]).css('display')
     }
     // ランダム数列に合わせて要素を割り当てる
     for (let step = 0; step < how_many_card; step++){
-      $($('.concentration--card')[step]).find('.concentration--text').text(textArr[randomArr[step]])
-      $($('.concentration--card')[step]).find('.concentration--text').css('color',colorArr[randomArr[step]])
-      $($('.concentration--card')[step]).find('.concentration--id').attr('id', idArr[randomArr[step]])
-      $($('.concentration--tail')[step]).css('display',displayArr[randomArr[step]])
+      $($('.card__cell')[step]).find('.concentration__text').text(textArr[randomArr[step]])
+      $($('.card__cell')[step]).find('.concentration__text').css('color',colorArr[randomArr[step]])
+      $($('.card__cell')[step]).find('.concentration__id').attr('id', idArr[randomArr[step]])
+      $($('.concentration__tail')[step]).css('display',displayArr[randomArr[step]])
     }
   }
   var isSelect = false
-  var how_many_card = $('.concentration--card').length
+  var how_many_card = $('.card__cell').length
   concentration_shuffle()// いきなりシャッフル
 
   $(function() {
     // カードを選択
-    $('.concentration--tail').on('click', function() {
+    $('.concentration__tail').on('click', function() {
       isSelect = !isSelect
       if (isSelect) {
         check_card = $(this)
         check_card.next().css('display','block')
-        $('.concentration--comment').text("カードが選択されています")
+        $('.concentration__comment').text("カードが選択されています")
       } else {
         $(this).next().css('display','block')
         // ペアがあったとき
-        if (($(this).next().find('.concentration--id').attr('id') == check_card.next().find('.concentration--id').attr('id'))) {
+        if (($(this).next().find('.concentration__id').attr('id') == check_card.next().find('.concentration__id').attr('id'))) {
           check_card.hide(1000)
           $(this).hide(1000)
           check_card.next().hide(1000)
           $(this).next().hide(1000)
-          $('.concentration--comment').text("揃いました！")
+          $('.concentration__comment').text("揃いました！")
         } else{
           setTimeout(()=>{
             check_card.next().css('display','none')
             $(this).next().css('display','none')
           }, 1000);
-          $('.concentration--comment').text("残念！")
+          $('.concentration__comment').text("残念！")
         }
       }
     })
@@ -68,14 +68,14 @@ document.addEventListener("turbolinks:load", function () {
       isSelect = false
       // 裏が残っているやつだけ、表をオープンにする
       for (let step = 0; step < how_many_card; step++){
-        if ($($('.concentration--card')[step]).find('.concentration--tail').css('display') != 'none'){
-          $($('.concentration--card')[step]).find('.concentration--head').css('display','block')
+        if ($($('.card__cell')[step]).find('.concentration__tail').css('display') != 'none'){
+          $($('.card__cell')[step]).find('.concentration__head').css('display','block')
         }
       }
-      $('.concentration--comment').text("カードをオープンしました")
+      $('.concentration__comment').text("カードをオープンしました")
       setTimeout(()=>{
         for (let step = 0; step < how_many_card; step++){
-          $($('.concentration--card')[step]).find('.concentration--head').css('display','none')
+          $($('.card__cell')[step]).find('.concentration__head').css('display','none')
         }
       }, 3000);
     })
